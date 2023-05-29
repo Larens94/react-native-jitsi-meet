f7373463d145fa3274bc2b38d36ca73ae3ea568d
Fabrizio Corpora <fabrizio.corpora@gmail.com>
2023-05-29 17:27:52 +0200

    ,,

--- index.android.js
+++ index.android.js
@@ -6,17 +6,18 @@ import { NativeModules, requireNativeComponent } from 'react-native';
 
 export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
 export const JitsiMeetModule = NativeModules.RNJitsiMeetModule
-const call = JitsiMeetModule.call;
-const endCall = JitsiMeetModule.endCall;
-JitsiMeetModule.call = (url, userInfo, meetOptions, meetFeatureFlags) => {
-  userInfo = userInfo || {};
-  meetOptions = meetOptions || {};
-  meetFeatureFlags = meetFeatureFlags || {};
-  call(url, userInfo, meetOptions, meetFeatureFlags);
-}
-JitsiMeetModule.endCall = () => {
-  endCall();
-}
+console.log('1....JitsiMeetModule:::', JitsiMeetModule)
+// const call = JitsiMeetModule.call;
+// const endCall = JitsiMeetModule.endCall;
+// JitsiMeetModule.call = (url, userInfo, meetOptions, meetFeatureFlags) => {
+//   userInfo = userInfo || {};
+//   meetOptions = meetOptions || {};
+//   meetFeatureFlags = meetFeatureFlags || {};
+//   call(url, userInfo, meetOptions, meetFeatureFlags);
+// }
+// JitsiMeetModule.endCall = () => {
+//   endCall();
+// }
 export default JitsiMeetModule;
 
 
